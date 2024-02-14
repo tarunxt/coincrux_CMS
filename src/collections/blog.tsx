4b8c817bd4554460944972dbc85ebe1a83d10006
kovid-sharma <76726175+kovid-sharma@users.noreply.github.com>
2024-02-14 05:26:31 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/collections/blog.tsx
@@ -0,0 +1,151 @@
+import { EnumValues, buildCollection, buildProperty } from "firecms";
+import { localeCollection } from "./locales.tsx";
+
+// Define the News type
+type News = {
+  coinDescription: string;
+  coinHeading: string;
+  coinImage: string;
+  createdAt: Date;
+  createdBy: string;
+  marketsCard: boolean;
+  assetName: string;
+  topicTitle: string;
+  category: string[];
+  totalDislikes: string[];
+  totalLikes: string[];
+};
+
+export const NewsCollection = buildCollection<News>({
+  name: "News",
+  singularName: "News",
+  path: "News",
+  icon: "News",
+  group: "News",
+  permissions: ({ authController, user }) => ({
+    read: true,
+    edit: true,
+    create: true,
+    delete: true,
+  }),
+  subcollections: [localeCollection],
+  properties: {
+    coinHeading: {
+      name: "Title",
+      validation: { required: true },
+      dataType: "string",
+    },
+    coinDescription: {
+      name: "Summary",
+      validation: { required: true },
+      dataType: "string",
+      columnWidth: 500,
+    },
+    coinImage: buildProperty({
+      name: "Image",
+      dataType: "string",
+      validation: { required: true },
+      storage: {
+        storagePath: "images",
+        acceptedFiles: ["image/*"],
+      },
+    }),
+    category: buildProperty({
+      name: 'Category',
+      validation: { required: true },
+      dataType: "array",
+      of: {
+        dataType: "string",
+      
+  
+      
+     
+      enumValues: {
+          "Banking": "Banking",
+          "Economy": "Economy",
+          "Environment": "Environment",
+          "Industry": "Industry",
+          "Infra": "Infra",
+          "IPO": "IPO",
+          "Markets": "Markets",
+          "Politics": "Politics",
+          "Science": "Science",
+          "Sports": "Sports",
+          "Stats": "Stats",
+          "Wealth": "Wealth",
+          "Bitcoin": "Bitcoin",
+          "Ethereum": "Ethereum",
+          "Analytics": "Analytics",
+          "Exchange": "Exchange",
+          // "Markets": "Markets",
+          "Metaverse": "Metaverse",
+          "Blockchain": "Blockchain",
+          "GameFi": "GameFi",
+          "Finance": "Finance",
+          "Others": "Others",
+          "Mining": "Mining",
+          "Security": "Security",
+          // "Economy": "Economy",
+          "World": "World",
+          "Legal": "Legal",
+          "Altcoins": "Altcoins"
+      },
+    },
+
+      ui: {
+        widget: "select",
+        showEnumIcon: true,
+      },
+  }),
+  
+  
+  
+  
+   
+  
+    assetName: buildProperty({
+      name: "Asset Name",
+      validation: { required: true },
+      dataType: "string",
+    }),
+    marketsCard: {
+      name: "Markets Card",
+      dataType: "boolean",
+    },
+    createdAt: buildProperty({
+      name: "Created on",
+      dataType: "date",
+      autoValue: "on_create",
+    }),
+    createdBy: {
+      name: "Created By",
+      validation: { required: true },
+      dataType: "string",
+      enumValues: {
+        Samridhi: "Samridhi Jain",
+        Yash: "Yash Jain",
+        Noopur: "Noopur Kumari",
+        Samiksha: "Samiksha Dhaka",
+        Anushka: "Ansuhka Verma",
+      },
+    },
+    topicTitle: {
+      name: "Topic Title",
+      dataType: "string",
+    },
+    totalLikes: {
+      name: "Likes",
+      dataType: "array",
+      of: {
+        dataType: "string",
+      },
+    },
+    totalDislikes: {
+      name: "Dislikes",
+      dataType: "array",
+      of: {
+        dataType: "string",
+      },
+    },
+  },
+});
---
+++ src/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4M4EtrppMgPbyGIhvN1ULc5SUeZSgy14",
+  authDomain: "coincrux-12be2.firebaseapp.com",
+  projectId: "coincrux-12be2",
+  storageBucket: "coincrux-12be2.appspot.com",
+  messagingSenderId: "87632180660",
+  appId: "1:87632180660:web:095dcbb457e80d7e4daee1",
+  measurementId: "G-9XB54QC4J1"
+};
